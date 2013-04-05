830cb64dd4d2a3f4b727985dd0ba52f4e209ec53
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2013-04-05 00:21:05 -0700

    Add in example

--- iOS/example/app.js
+++ iOS/example/app.js
@@ -1,39 +1,69 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
-
-
-// open a single window
+/**
+ * This example demonstrates how to zip and unzip files.
+ *
+ * We'll demonstrate this in two different ways:
+ *  - Zipping files.
+ *  - Unzipping an archive.
+ */
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+    backgroundColor: 'white'
 });
-var label = Ti.UI.createLabel();
-win.add(label);
 win.open();
 
-// TODO: write your module tests here
-var Zip = require('bencoding.zip');
-Ti.API.info("module is => " + Zip);
+var Compression = require('bencoding.zip');
+var outputDirectory = Ti.Filesystem.applicationDataDirectory;
+var inputDirectory = Ti.Filesystem.resourcesDirectory + 'data/';
 
-label.text = Zip.example();
+/**
+ * The following lines zip the a.txt and b.txt from the "data"
+ * directory in your resources to the data directory of your app.
+ */
+var zipFiles = Ti.UI.createButton({
+    title: 'Zip a.txt and b.txt',
+    top: 20, left: 20, right: 20,
+    height: 40
+});
+function onComplete(e){
+	Ti.API.info(JSON.stringify(e));
+};
+zipFiles.addEventListener('click', function () {
+    var writeToZip = outputDirectory + '/zipFiles.zip';
 
-Ti.API.info("module exampleProp is => " + Zip.exampleProp);
-Zip.exampleProp = "This is a test value";
+    Compression.zip({
+    		zip: writeToZip, 
+    		password:"foo",
+    		files: [inputDirectory + 'a.txt',
+        	inputDirectory + 'b.txt'],
+        	completed:onComplete
+    });
+});
+win.add(zipFiles);
 
-if (Ti.Platform.name == "android") {
-	var proxy = Zip.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
+/**
+ * The following lines extract the contents of the "a+b.zip" file
+ * from the "data" directory in your resources to the data directory
+ * of your app.
+ */
+var unzipArchive = Ti.UI.createButton({
+    title: 'Unzip ab.zip',
+    top: 80, left: 20, right: 20,
+    height: 40
+});
+unzipArchive.addEventListener('click', function () {
+    var zipFileName = inputDirectory + 'ab.zip';
+    Compression.unzip({
+    	outputDirectory:outputDirectory, 
+    	zip:zipFileName, 
+    	overwrite:true,
+    	completed:onComplete
+    });
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
+});
+win.add(unzipArchive);
 
+var status = Ti.UI.createLabel({
+    text: 'Hit one of the buttons above, and the result will display here.',
+    color: '#333',
+    top: 140, left: 20, right: 20, bottom: 20
+});
+win.add(status);
